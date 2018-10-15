454a1b737e32a4b6ec24873ac93f431cb19df2c5
FredBF <frfle15@student.sdu.dk>
2018-10-15 10:35:38 +0200

    used adaptive median filter

--- image2.cpp
+++ image2.cpp
@@ -4,7 +4,11 @@ int main()
 {
     Mat img2 = imread("../Images/Image2.png", IMREAD_GRAYSCALE);
     
-    Analysis("2", img2, false);
+    //Analysis("2", img2, false);
+
+    Mat resA;
+    FredsAdaptiveMedian(img2, resA, 9);
+    Analysis("A", resA, true);
     
     waitKey(0);
     return 0;
