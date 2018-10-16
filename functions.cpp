61bba91d5c9c52179ef826159915374083812a8e
FredBF <frfle15@student.sdu.dk>
2018-10-16 10:00:33 +0200

    fixed errors

--- functions.cpp
+++ functions.cpp
@@ -398,7 +398,7 @@ void ApplyFreqFilter(Mat &anImage, Mat &aFilter)
     dft(img2Channel, img2Channel, DFT_COMPLEX_OUTPUT);
 
     //shift the filter quadrants
-    dftshift(filter);
+    dftshift(aFilter);
 
     //multiply in frequency domain to apply filter
     Mat resFreq;
@@ -410,7 +410,7 @@ void ApplyFreqFilter(Mat &anImage, Mat &aFilter)
     dft(resFreq, res, DFT_INVERSE + DFT_SCALE + DFT_REAL_OUTPUT);
 
     //crop to remove padding
-    res = Mat(res, Rect(Point(0, 0), img.size()));
+    res = Mat(res, Rect(Point(0, 0), anImage.size()));
 
     //save
     res.copyTo(anImage);
